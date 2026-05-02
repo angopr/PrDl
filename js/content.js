9f4821710321d96c656866097fa8a896820d0ba3
exoticsnipes13-cmyk <exoticsnipes13@gmail.com>
2026-05-02 16:20:50 -0400

    Update content.js

--- js/content.js
+++ js/content.js
@@ -3,7 +3,7 @@ import { round, score } from './score.js';
 /**
  * Path to directory containing `_list.json` and all levels
  */
-const dir = '/data';
+const dir = './data';
 
 export async function fetchList() {
     const listResult = await fetch(`${dir}/_list.json`);
@@ -48,6 +48,7 @@ export async function fetchEditors() {
 
 export async function fetchLeaderboard() {
     const list = await fetchList();
+    if (!list) return [[], ['Failed to load data']];
 
     const scoreMap = {};
     const errs = [];
